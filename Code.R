f1b15788d06533f4a66f269a716e4158f9f9a9fe
johriashish28 <42682659+johriashish28@users.noreply.github.com>
2020-06-15 13:45:38 +0200

    Add files via upload

--- Code.R
+++ Code.R
@@ -1,30 +1,27 @@
 setwd("C:/Users/S.K/Desktop/Analytics_Vidya_Projects/Loan_Prediction")
 
-DS<- read.csv("Train_Data.csv")
+train<- read.csv("Train_Data.csv")
 test<- read.csv("Test_Data.csv")
+
+#Removing Blanks from the train dataset
+X<- which(!complete.cases(train))
+DS<- train[-X,]
 View(DS)
 
-test<- test[,-1]
-
-X<- which(!complete.cases(DS))
-DS1<- DS[-X,]
-View(DS1)
-
-
-DSNEW <- DS1[,2:13]
-View(DSNEW)
 
 library(plyr)
 
-#str(DSNEW)
+#to check the structure of all variables
+str(DSNEW)
 
-DSNEW$Gender = revalue(DSNEW$Gender, c("Male"="1", "Female"="2"))
-DSNEW$Married = revalue(DSNEW$Married, c("Yes"="1", "No"="0"))
-DSNEW$Dependents = revalue(DSNEW$Dependents, c("0"="0", "1"="1", "2"="2", "3+"="3"))
-DSNEW$Education = revalue(DSNEW$Education, c("Graduate"="1", "Not Graduate"="2"))
-DSNEW$Self_Employed = revalue(DSNEW$Self_Employed, c("Yes"="1", "No"="0"))
-DSNEW$Property_Area = revalue(DSNEW$Property_Area, c("Urban"="1", "Semiurban"="2", "Rural"="3"))
-DSNEW$Loan_Status = revalue(DSNEW$Loan_Status, c("Y"="1", "N"="0"))
+#Recoding of factor variables
+DS$Gender = revalue(DS$Gender, c("Male"="1", "Female"="2"))
+DS$Married = revalue(DS$Married, c("Yes"="1", "No"="0"))
+DS$Dependents = revalue(DS$Dependents, c("0"="0", "1"="1", "2"="2", "3+"="3"))
+DS$Education = revalue(DS$Education, c("Graduate"="1", "Not Graduate"="2"))
+DS$Self_Employed = revalue(DS$Self_Employed, c("Yes"="1", "No"="0"))
+DS$Property_Area = revalue(DS$Property_Area, c("Urban"="1", "Semiurban"="2", "Rural"="3"))
+DS$Loan_Status = revalue(DS$Loan_Status, c("Y"="1", "N"="0"))
 
 test$Gender = revalue(test$Gender, c("Male"="1", "Female"="2"))
 test$Married = revalue(test$Married, c("Yes"="1", "No"="0"))
@@ -47,24 +44,25 @@ test$Property_Area = revalue(test$Property_Area, c("Urban"="1", "Semiurban"="2",
 #train[,1:2] = scale(train[,1:2])
 
 
+
 #str(DSNEW)
 
 #XYZ<-scale(DSNEW)
 #model <- glm(Loan_Status ~ . ,train,family = "binomial")
 model <- glm(formula = Loan_Status ~ Credit_History + Property_Area ,
              family = binomial,
-             data = DSNEW)
+             data = DS)
 summary(model)
 
 
 #table(DSNEW$Loan_Status, DSNEW$Gender)
 
-proc_pred = predict(model, type = 'response', newdata = DSNEW)
-DSNEW$Pre = ifelse(proc_pred > 0.5, 1, 0)
+proc_pred = predict(model, type = 'response', newdata = DS)
+DS$Pred = ifelse(proc_pred > 0.5, 1, 0)
 
 
 #Confusion Matrix
-tab1<- table(Predicted = DSNEW$Pre, Actual = DSNEW$Loan_Status)
+tab1<- table(Predicted = DS$Pred, Actual = DS$Loan_Status)
 tab1
 
 #Misclassification Error
@@ -72,6 +70,6 @@ tab1
 
 
 proc_pred = predict(model, type = 'response', newdata = test)
-test$Pre = ifelse(proc_pred > 0.5, 1, 0)
+test$Pred = ifelse(proc_pred > 0.5, 1, 0)
 
 write.csv(test, "test.csv", row.names = FALSE)
